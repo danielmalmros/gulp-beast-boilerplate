47aef9c6565720a571ac56f0e2f0944ab7097677
Daniel Malmros <daniel@bymalmros.dk>
2016-09-20 21:09:06 +0200

    first commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -801,33 +801,49 @@ References:
 		}
 	};
 })(this);
-// List render, laver listen til tid og dato
-var timeDate = new Vue({
-    el: '#time-date',
+var app = new Vue({
+    el: '#app',
     data: {
-        times: [
-            {message: '21:30'},
-            {message: '04-09-2016'}
-        ]
+        name: '',
+        errors: '',
+        newExtra: '',
+        extras: [{
+            name: 'Daniel Malmros'
+        }]
+    },
+    methods: {
+        addExtra: function() {
+            var text = this.newExtra.trim()
+            if (text) {
+                this.extras.push({
+                    name: text
+                })
+                this.newExtra = '';
+            }
+        },
+        // removeTodo: function(index) {
+        //     this.todos.splice(index, 1)
+        // }
+    },
+    ready: function() {
+        // GET request with ES6
+        this.$http.get('http://private-3f637-vuetest1.apiary-mock.com/questions').then((response) => {
+            this.errors = response.data.errors;
+        }, (response) => {
+            // error callback
+        });
+
+        // GET request
+        // this.$http({
+        //     url: 'https://baditicker.herokuapp.com/',
+        //     method: 'GET'
+        // }).then(function(response) {
+        //     this.pools = response.data.pools;
+        // }, function(response) {
+        //     // error callback
+        // });
     }
 });
 
 
-// List render, laver listen til klasse information
-var className = new Vue({
-    el: '#class',
-    data: {
-        classes: [
-            {message: 'Digital Development Interaction'},
-            {message: '13:00 - 14:30'},
-            {message: 'Admin Pronestor'}
-        ]
-    }
-});
-
-new Vue({
-    el: '.room-number',
-    data: {
-        message: 'S.30'
-    }
-});
+// http://jsonplaceholder.typicode.com/users
